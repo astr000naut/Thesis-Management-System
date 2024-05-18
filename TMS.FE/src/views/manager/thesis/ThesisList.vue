dab0b76e941fb35be152a12ecb89c55c7fbd4463
duxngnt <19020040@vnu.edu.vn>
2024-05-19 02:59:18 +0700

    update text

--- TMS.FE/src/views/manager/thesis/ThesisList.vue
+++ TMS.FE/src/views/manager/thesis/ThesisList.vue
@@ -8,7 +8,7 @@
         />
         <div class="page__header flex-row">
             <h1 class="page__title" style="font-size: 24px">
-                Danh sách khóa luận đang thực hiện
+                Danh sách khóa luận
             </h1>
         </div>
         <div class="search-container flex-row al-center cg-2">
